22d11211642d0a3b478115d5717836a59cc59505
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-05-08 23:06:37 -0500

    bullet sound

--- Assets/Assets/Scripts/BulletController.cs
+++ Assets/Assets/Scripts/BulletController.cs
@@ -8,23 +8,47 @@ public class BulletController : MonoBehaviour
     [SerializeField] private Rigidbody2D myRGB2D;
     [SerializeField] private float velocityMultiplier;
     [SerializeField] private int damage;
+    [SerializeField] private AudioClip shootSound; // AudioClip para el sonido de disparo
+    private AudioSource audioSource; // Componente AudioSource para reproducir el sonido
+
     public event Action<int, HealthBarController> onCollision;
 
-    public void SetUpVelocity(Vector2 velocity, string newTag){
+    private void Awake()
+    {
+        // Obtener o añadir un componente AudioSource al objeto
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void SetUpVelocity(Vector2 velocity, string newTag)
+    {
         myRGB2D.velocity = velocity * velocityMultiplier;
         gameObject.tag = newTag;
 
         DamageManager.instance.SubscribeFunction(this);
+
+        // Reproducir el sonido de disparo
+        if (shootSound != null)
+        {
+            audioSource.PlayOneShot(shootSound);
+        }
     }
 
-    private void OnBecameInvisible() {
+    private void OnBecameInvisible()
+    {
         Destroy(this.gameObject);
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(!other.CompareTag(gameObject.tag) && (other.CompareTag("Player") || other.CompareTag("Enemy"))){
-            if(other.GetComponent<HealthBarController>()){
-                onCollision?.Invoke(damage,other.GetComponent<HealthBarController>());
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag(gameObject.tag) && (other.CompareTag("Player") || other.CompareTag("Enemy")))
+        {
+            if (other.GetComponent<HealthBarController>())
+            {
+                onCollision?.Invoke(damage, other.GetComponent<HealthBarController>());
             }
             Destroy(this.gameObject);
         }

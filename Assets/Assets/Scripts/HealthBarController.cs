c88f22b47a7438f11e3e41f4552ace87a2787aad
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-05-08 23:16:16 -0500

    husrt sound

--- Assets/Assets/Scripts/HealthBarController.cs
+++ Assets/Assets/Scripts/HealthBarController.cs
@@ -6,10 +6,12 @@ using System;
 public class HealthBarController : MonoBehaviour
 {
     [SerializeField] private int maxValue;
-    [Header("Health Bar Visual Components")] 
+    [Header("Health Bar Visual Components")]
     [SerializeField] private RectTransform healthBar;
     [SerializeField] private RectTransform modifiedBar;
     [SerializeField] private float changeSpeed;
+    [SerializeField] private AudioClip hitSound; // AudioClip para el sonido de golpe
+    private AudioSource audioSource; // Componente AudioSource para reproducir el sonido
 
     private int currentValue;
     private float _fullWidth;
@@ -19,7 +21,18 @@ public class HealthBarController : MonoBehaviour
     public event Action onHit;
     public event Action onDeath;
 
-    private void Start() {
+    private void Awake()
+    {
+        // Obtener o añadir un componente AudioSource al objeto
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    private void Start()
+    {
         currentValue = maxValue;
         _fullWidth = healthBar.rect.width;
     }
@@ -28,35 +41,47 @@ public class HealthBarController : MonoBehaviour
     /// Metodo <c>UpdateHealth</c> actualiza la vida del personaje de manera visual. Recibe una cantidad de vida modificada.
     /// </summary>
     /// <param name="amount">El valor de vida modificada.</param>
-    public void UpdateHealth(int amount){
+    public void UpdateHealth(int amount)
+    {
         currentValue = Mathf.Clamp(currentValue + amount, 0, maxValue);
         onHit?.Invoke();
 
-        if(updateHealthBarCoroutine != null){
+        // Reproducir el sonido de golpe
+        if (amount < 0 && hitSound != null)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+
+        if (updateHealthBarCoroutine != null)
+        {
             StopCoroutine(updateHealthBarCoroutine);
         }
         updateHealthBarCoroutine = StartCoroutine(AdjustWidthBar(amount));
 
-        if(currentValue == 0){
+        if (currentValue == 0)
+        {
             onDeath?.Invoke();
         }
     }
 
-    IEnumerator AdjustWidthBar(int amount){
+    IEnumerator AdjustWidthBar(int amount)
+    {
         RectTransform targetBar = amount >= 0 ? modifiedBar : healthBar;
         RectTransform animatedBar = amount >= 0 ? healthBar : modifiedBar;
 
-        targetBar.sizeDelta = SetWidth(targetBar,TargetWidth);
+        targetBar.sizeDelta = SetWidth(targetBar, TargetWidth);
 
-        while(Mathf.Abs(targetBar.rect.width - animatedBar.rect.width) > 1f){
-            animatedBar.sizeDelta = SetWidth(animatedBar,Mathf.Lerp(animatedBar.rect.width, TargetWidth, Time.deltaTime * changeSpeed));
+        while (Mathf.Abs(targetBar.rect.width - animatedBar.rect.width) > 1f)
+        {
+            animatedBar.sizeDelta = SetWidth(animatedBar, Mathf.Lerp(animatedBar.rect.width, TargetWidth, Time.deltaTime * changeSpeed));
             yield return null;
         }
 
-        animatedBar.sizeDelta = SetWidth(animatedBar,TargetWidth);
+        animatedBar.sizeDelta = SetWidth(animatedBar, TargetWidth);
     }
 
-    private Vector2 SetWidth(RectTransform t, float width){
+    private Vector2 SetWidth(RectTransform t, float width)
+    {
         return new Vector2(width, t.rect.height);
     }
 }
--- Assets/Assets/Scripts/PlayerController.cs
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -12,11 +12,39 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private BulletController bulletPrefab;
     [SerializeField] private CameraController cameraReference;
 
-    private void Start() {
-        GetComponent<HealthBarController>().onHit += cameraReference.CallScreenShake;
+    private AudioSource audioSource; // Componente AudioSource para reproducir el sonido
+
+    private void Awake()
+    {
+        // Obtener o añadir un componente AudioSource al objeto
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
     }
 
-    private void Update() {
+    private void Start()
+    {
+        HealthBarController healthBarController = GetComponent<HealthBarController>();
+        healthBarController.onHit += OnHit;
+        healthBarController.onHit += cameraReference.CallScreenShake;
+    }
+
+    private void OnHit()
+    {
+        // Reproducir el sonido de golpe
+        // Aquí debes asignar el AudioClip adecuado para el sonido de golpe
+        // Puedes asignarlo desde el Inspector de Unity
+        AudioClip hitSound = null; // Asigna el AudioClip para el sonido de golpe
+        if (hitSound != null)
+        {
+            audioSource.PlayOneShot(hitSound);
+        }
+    }
+
+    private void Update()
+    {
         Vector2 movementPlayer = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         myRBD2.velocity = movementPlayer * velocityModifier;
 
@@ -25,19 +53,23 @@ public class PlayerController : MonoBehaviour
         Vector3 mouseInput = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         CheckFlip(mouseInput.x);
-    
+
         Vector3 distance = mouseInput - transform.position;
         Debug.DrawRay(transform.position, distance * rayDistance, Color.red);
 
-        if(Input.GetMouseButtonDown(0)){
-            BulletController myBullet =  Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        if (Input.GetMouseButtonDown(0))
+        {
+            BulletController myBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
             myBullet.SetUpVelocity(distance.normalized, gameObject.tag);
-        }else if(Input.GetMouseButtonDown(1)){
-            Debug.Log("Left Click");
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right Click");
         }
     }
 
-    private void CheckFlip(float x_Position){
+    private void CheckFlip(float x_Position)
+    {
         spriteRenderer.flipX = (x_Position - transform.position.x) < 0;
     }
 }
